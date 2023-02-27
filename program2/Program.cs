5fe2a75cea4d41d042055fafdbbbad85979ae8ad
NSergeevich <niknikitaser@gmail.com>
2023-02-27 17:55:01 +0100

    practicum

---
+++ program2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+System.Console.Write("Vvedite chislo 1: ");
+
+string data; 
+data = Console.ReadLine();
+
+int a = Convert.ToInt32(data);
+
+System.Console.Write("Vvedite chislo 2: ");
+
+data = Console.ReadLine();
+
+int b = Convert.ToInt32(data);
+
+int result = a + b;
+
+System.Console.WriteLine("Summa chisel = " + result);
