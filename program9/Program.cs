575031d45386b789c470c21235d3f8c4ecdd1d2a
NSergeevich <niknikitaser@gmail.com>
2023-03-31 17:45:41 +0200

    deberes

---
+++ program9/Program.cs
@@ -0,0 +1,33 @@
+﻿// Заполнить массив с клавиатуры
+// Вывести массив в обратном порядке
+// Найти сумму четных чисел в массиве
+// Найти наименьшее число в массиве
+
+int[] newArray = new int[] { 1, 2, 3, -4, 5, 6, 7, -8, 9 };
+
+int len = newArray.Length;
+System.Console.WriteLine(len);
+
+int sum = 0;
+int minNumber = newArray[0];
+
+for (int i = newArray.Length - 1; i >= 0; i--)
+{
+    System.Console.Write($"{newArray[i]} ");
+}
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    if (newArray[i] %2 == 0)
+    {
+        sum += newArray[i];
+    }
+}
+System.Console.WriteLine();
+System.Console.WriteLine(sum);
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    if (newArray[i] < minNumber) minNumber = newArray[i];
+}
+System.Console.WriteLine(minNumber);
