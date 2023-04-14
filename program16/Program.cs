352a00e7266ad7de583d815bce35a5b9fe87a35d
NSergeevich87 <niknikitaser@gmail.com>
2023-04-14 20:30:52 +0200

    newpractice

---
+++ program16/Program.cs
@@ -0,0 +1,15 @@
+﻿string message = Console.ReadLine();
+double.TryParse(message, out double result);
+System.Console.WriteLine(result);
+
+void Foo(params object[] parameters)
+{
+    string message = "Тип данных {0}, значение {1}";
+
+    foreach (var item in parameters)
+    {
+        System.Console.WriteLine(message, item.GetType(), item);
+    }
+}
+
+Foo("Hello World", '$', 5.55, 8f, 9);
