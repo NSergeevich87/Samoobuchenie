c36850dd9001198ef52e95e1a73231f45bfa974b
NSergeevich87 <niknikitaser@gmail.com>
2023-05-17 09:19:22 +0200

    bien task

--- Program.cs
+++ Program.cs
@@ -1,51 +1,76 @@
-﻿using System;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Clase_77
+﻿namespace Clase_82
 {
-    public class ClassPoint
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public void Print()
-        {
-            Console.WriteLine($"X:{X}\tY:{Y}");
-        }
-
-    }
-
-    public struct StructPoint
-    { 
-        public int X { get; set; } 
-        public int Y { get; set; } 
-
-        public void Print() 
-        {
-            Console.WriteLine($"X:{X}\tY:{Y}");
-        }
-    }
+    /*
+    Kata.HighAndLow("1 2 3 4 5");  // return "5 1"
+    Kata.HighAndLow("1 2 -3 4 5"); // return "5 -3"
+    Kata.HighAndLow("1 9 3 4 -5"); // return "9 -5"
+    */
 
     class Program
     {
-        static void Foo(ClassPoint classPoint) 
+        public static void Main(string[] args)
         {
-            classPoint.X++;
-            classPoint.Y++;
+            string test = "what time are we climbingsdhthr up to the volcano"; //taxi 54
+            Console.WriteLine(High(test));
         }
-        static void Bar(StructPoint structPoint) 
+        public static string High(string s)
         {
-            structPoint.X++;
-            structPoint.Y++;
-        }
-        static void Main(string[] args) 
-        {
-            ClassPoint classPoint = new ClassPoint();
-            StructPoint structPoint = new StructPoint();
+            char[] mesArr = s.ToLower().ToCharArray();
 
-            Foo(classPoint);
-            Bar(structPoint);
+            string alf = "abcdefghijklmnopqrstuvwxyz";
+            char[] alfArr = alf.ToCharArray();
+
+            string word = String.Empty;
+            int maxSum = 0;
+            int sum = 0;
+            string newWord = String.Empty;
+
+            for (int i = 0; i < mesArr.Length; i++)
+            {
+                if (mesArr[i] == ' ')
+                {
+                    i++;
+                    if (maxSum < sum)
+                    {
+                        maxSum = sum;
+                        newWord = word;
+                    }
+                    sum = 0;
+                    word = "";
+                }
+                for (int j = 0; j < alfArr.Length; j++)
+                {
+                    if (i == mesArr.Length - 1)
+                    {
+                        if (mesArr[i] == alfArr[j])
+                        {
+                            sum += j + 1;
+                            word += mesArr[i];
+                            if (maxSum < sum)
+                            {
+                                maxSum = sum;
+                                newWord = word;
+                            }
+                            break;
+                        }
+                    }
+                    else if (mesArr[i] == alfArr[j])
+                    {
+                        sum += j + 1;
+                        word += mesArr[i];
+                        break;
+                    }
+                }
+            }
+            return newWord;
+        }
+
+        public static void PrintArr(int[] array)
+        {
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                Console.Write(array[i]);
+            }
         }
     }
-
 }
